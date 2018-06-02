1480644dcc1da24190f15d162b32ac4866fb64c1
priyamoorthy <36322713+priyamoorthy@users.noreply.github.com>
2018-06-02 07:53:32 -0700

    kvalue

---
+++ kvalue.py
@@ -0,0 +1,14 @@
+n=int(input("enter the number of elements in the list "))
+K=int(input("k"))
+list=[]
+for i in range(1,n+1):
+  a=int(input("input value"))
+  list.append(a)
+for i in range(1,N+1):
+  if (list[i]==K):
+    print ("yes")
+    break
+  else:
+    print("no")
+    break
+
