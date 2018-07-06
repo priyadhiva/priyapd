64b7d2ee0b12e7fd6067d94b5e545dec5e99486d
priyamoorthy <36322713+priyamoorthy@users.noreply.github.com>
2018-07-06 20:53:48 +0530

    arithmetic

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,5 @@
 a=int(input("enter the value of a"))
-n=int(input("nter the value of n"))
+n=int(input("enter the value of n"))
 d=int(input("enter the difference d"))
 AP=a+(n-1)*d
 print("the sum of AP of N terms is",AP)
