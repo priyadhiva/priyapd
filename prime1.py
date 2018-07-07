0c426d76e2bbcede6160d36dade047d128a2d476
priyamoorthy <36322713+priyamoorthy@users.noreply.github.com>
2018-07-07 10:27:12 +0530

    prime1

--- prime1.py
+++ prime1.py
@@ -1,6 +1,6 @@
 x=int(input("enter a number1"))
 y=int(input("enter a number2"))
-for num in range(1,y+1) :
+for num in range(x,y+1) :
     if num>1:
         for i in range(2,num):
             if(num%i)==0:
